e20f357fe957b1c75b42b1a9c0a6a232cb0a25ce
HidaYAtMus <rio.mus48@gmail.com>
2020-01-17 10:23:29 +0700

    test

---
+++ pindad.js
@@ -0,0 +1,39 @@
+const webdriver = require('selenium-webdriver'), 
+io = require("socket.io-client"),
+ioClient = io.connect("http://10.10.2.45");
+
+// ioClient.emit('pesan', function(msg){
+    // socket.emit ('judul').append(text(title));
+// });
+
+function pindad(){
+    var driver = new webdriver.Builder().forBrowser('chrome').build();
+
+    driver.get('https://pindad.com/').then(function(){
+    driver.findElement(webdriver.By.css(".main-navigation > .submeny:nth-child(2) .scroll-to-link:nth-child(1) > a")).click().then(function(){
+            driver.findElement(webdriver.By.css(".entry:nth-child(2)")).click()
+            function sleep(){
+                driver.sleep(2000).then(function() {
+                    driver.getTitle().then(function(title) {
+                      if(title === title) {
+                        console.log('Test passed = ', title);
+                        ioClient.emit ('judul', title);
+                        var json = {nama_web : [title],
+                                    hasil : ["passed"]
+                                  }
+                        
+                        console.log(json);
+                        ioClient.emit('kirim', json);
+                      } else {
+                        console.log('Test failed =', title);
+                        ioClient.emit ('judul', 'gagal');
+                    }
+                      driver.quit();
+                    });
+                  });
+                }
+                sleep();
+    });
+});    
+}
+pindad();
