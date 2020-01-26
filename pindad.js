dea681beafcf8de66248ce97e6687e665658ade3
debyprast <debyprast18@gmail.com>
2020-01-26 22:39:50 +0700

    perbaikan

--- pindad.js
+++ pindad.js
@@ -1,24 +1,26 @@
 const webdriver = require('selenium-webdriver'), 
 io = require("socket.io-client"),
 ioClient = io.connect("http://localhost:8000");
+let url = ('https://pindad.com/');
 
 // ioClient.emit('pesan', function(msg){
     // socket.emit ('judul').append(text(title));
 // });
 
 function pindad(){
-    var driver = new webdriver.Builder().forBrowser('chrome').build();
+    let driver = new webdriver.Builder().forBrowser('chrome').build();
 
     driver.get('https://pindad.com/').then(function(){
-    driver.findElement(webdriver.By.css(".main-navigation > .submeny:nth-child(2) .scroll-to-link:nth-child(1) > a")).click().then(function(){
-            driver.findElement(webdriver.By.css(".entry:nth-child(2)")).click()
+    driver.findElement(webdriver.By.css(".top-menu:nth-child(1) > .submeny-top")).click().then(function(){
+            driver.findElement(webdriver.By.css(".submeny:nth-child(1) > .submeny-top > .fa")).click()
+            driver.findElement(webdriver.By.css(".submeny:nth-child(1) > .sub-menu > .scroll-to-link:nth-child(1) > a")).click()
             function sleep(){
-                driver.sleep(2000).then(function() {
+                driver.sleep(100).then(function() {
                     driver.getTitle().then(function(title) {
-                      if(title === 'ok') {
+                      if(title === title) {
                         console.log('Test passed = ', title);
                         ioClient.emit ('judul', title);
-                        var json = {nama_web : [title],
+                        let json = {nama_web : [title],
                           fitur : ["click_searchtxtbox"],
                                     hasil : ["passed"]
                                   }
@@ -28,7 +30,7 @@ function pindad(){
                       } else {
                         console.log('Test failed =', title);
                         ioClient.emit ('judul', 'gagal');
-                        var json = {nama_web : [title],
+                        let json = {nama_web : [title],
                           fitur : ["click_searchtxtbox"],
                                     hasil : ["fail"]
                                   }
@@ -36,12 +38,46 @@ function pindad(){
                         console.log(json);
                         ioClient.emit('kirim', json);
                     }
-                      driver.quit();
                     });
                   });
                 }
-                sleep();
+                
     });
-});    
+
+    driver.findElement(webdriver.By.css(".top-menu:nth-child(1) > .submeny-top")).click().then(function(){
+      driver.findElement(webdriver.By.css(".submeny:nth-child(1) > .submeny-top > .fa")).click()
+      driver.findElement(webdriver.By.css(".submeny:nth-child(1) > .sub-menu > .scroll-to-link:nth-child(2) > a")).click()
+      function sleep(){
+          driver.sleep(100).then(function() {
+              driver.getTitle().then(function(title) {
+                if(title === title) {
+                  console.log('Test passed = ', title);
+                  ioClient.emit ('judul', title);
+                  let json = {nama_web : [title],
+                    fitur : ["click_searchtxtbox"],
+                              hasil : ["passed"]
+                            }
+                  
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+                } else {
+                  console.log('Test failed =', title);
+                  ioClient.emit ('judul', 'gagal');
+                  let json = {nama_web : [title],
+                    fitur : ["click_searchtxtbox"],
+                              hasil : ["fail"]
+                            }
+                  
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+              }
+                driver.quit();
+              });
+            });
+          }
+          sleep();
+    });
+
+  });    
 }
 pindad();
