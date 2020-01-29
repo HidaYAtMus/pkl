f797de6999c07ffe6724bc94fd41ec4147654108
debyprast <debyprast18@gmail.com>
2020-01-29 13:13:38 +0700

    penyelesaian

--- bts.js
+++ bts.js
@@ -1,118 +1,237 @@
 const webdriver = require('selenium-webdriver'), 
 io = require("socket.io-client"),
 ioClient = io.connect("http://autotest.divmu.pindad.co.id/");
+let nama = ('http://bts.divmu.pindad.co.id/');
 
 function pindad(){
     var driver = new webdriver.Builder().forBrowser('chrome').build();
-
+    function sleep(){
+        driver.sleep(90000);
+    }
     driver.get('http://bts.divmu.pindad.co.id/');
     driver.findElement(webdriver.By.linkText("Register")).click().then(function(){
-        driver.findElement(webdriver.By.name("username")).sendKeys("testing");
-        driver.findElement(webdriver.By.name("password")).sendKeys("testing");
-        driver.findElement(webdriver.By.name("nama")).sendKeys("testing");
-        driver.findElement(webdriver.By.name("nik")).sendKeys("3456");
-        driver.findElement(webdriver.By.name("tgl_lahir")).click("label=17");
-        driver.findElement(webdriver.By.name("bln_lahir")).click("label=Okt");
-        driver.findElement(webdriver.By.name("thn_lahir")).click("label=1989");
-        driver.findElement(webdriver.By.name("jenis_kelamin")).click();
-        driver.findElement(webdriver.By.name("pekerjaan")).sendKeys("mahasiswa");
-        driver.findElement(webdriver.By.name("alamat")).sendKeys("smw");
-        driver.findElement(webdriver.By.name("email")).sendKeys("g@gmail.com");
-        driver.findElement(webdriver.By.name("no_hp")).sendKeys("08729279"),
-        driver.findElement(webdriver.By.name("reset")).click().then(function(){
-                  driver.getTitle().then(function(title) {
-                    if(title === title) {
-                        console.log('Test passed = ', title);
-                        ioClient.emit ('judul', title);
-                        var json = {nama_web : ["http://bts.divmu.pindad.co.id/"],
-                                    fitur : ["LoginUser"],
-                                    hasil : ["passed"]
-                                   }
-
-                         console.log(json);
-                         ioClient.emit('kirim', json);
-                         } else {
-                          console.log('Test failed =', title);
-                          ioClient.emit ('judul', title);
-                          var json = {nama_web : ["http://bts.divmu.pindad.co.id/"],
-                                      fitur : ["loginUser"],
-                                      hasil : ["fail"]
-                          }
-                          console.log(json);
-                          ioClient.emit('kirim', json);
-                         }
-                            // driver.quit();
-        });
-        driver.findElement(webdriver.By.name("username")).sendKeys("testing");
-        driver.findElement(webdriver.By.name("password")).sendKeys("testing");
-        driver.findElement(webdriver.By.name("nama")).sendKeys("testing");
-        driver.findElement(webdriver.By.name("nik")).sendKeys("3456");
-        driver.findElement(webdriver.By.name("tgl_lahir")).click("label=17");
-        driver.findElement(webdriver.By.name("bln_lahir")).click("label=Okt");
-        driver.findElement(webdriver.By.name("thn_lahir")).click("label=1989");
-        driver.findElement(webdriver.By.name("jenis_kelamin")).click();
-        driver.findElement(webdriver.By.name("pekerjaan")).sendKeys("mahasiswa");
-        driver.findElement(webdriver.By.name("alamat")).sendKeys("smw");
-        driver.findElement(webdriver.By.name("email")).sendKeys("g@gmail.com");
-        driver.findElement(webdriver.By.name("no_hp")).sendKeys("08729279"),
-        driver.findElement(webdriver.By.name("Submit")).click().then(function(){
-                  driver.getTitle().then(function(title) {
-                    if(title === title) {
-                        console.log('Test passed = ', title);
-                        ioClient.emit ('judul', title);
-                        var json = {nama_web : ["http://bts.divmu.pindad.co.id/"],
-                                    fitur : ["LoginUser"],
-                                    hasil : ["passed"]
-                                   }
-
-                         console.log(json);
-                         ioClient.emit('kirim', json);
-                         } else {
-                          console.log('Test failed =', title);
-                          ioClient.emit ('judul', title);
-                          var json = {nama_web : ["http://bts.divmu.pindad.co.id/"],
-                                      fitur : ["loginUser"],
-                                      hasil : ["fail"]
-                          }
-                          console.log(json);
-                          ioClient.emit('kirim', json);
-                         }
-                            // driver.quit();
-        });
 
         driver.findElement(webdriver.By.linkText("Login")).click().then(function(){
             driver.findElement(webdriver.By.name("username")).sendKeys("19216819700020");
+            sleep()
             driver.findElement(webdriver.By.name("password")).sendKeys("rizky");
+            sleep()
             driver.findElement(webdriver.By.css("input:nth-child(6)")).click().then(function(){
-                
+            sleep()
             driver.getTitle().then(function(title) {
                 if(title === title) {
-                    console.log('Test passed = ', title);
-                    ioClient.emit ('judul', title);
-                    var json = {nama_web : ["http://bts.divmu.pindad.co.id/"],
-                                fitur : ["LoginUser"],
-                                hasil : ["passed"]
-                               }
-
-                     console.log(json);
-                     ioClient.emit('kirim', json);
-                     } else {
-                      console.log('Test failed =', title);
-                      ioClient.emit ('judul', title);
-                      var json = {nama_web : ["http://bts.divmu.pindad.co.id/"],
-                                  fitur : ["loginUser"],
-                                  hasil : ["fail"]
-                      }
-                      console.log(json);
-                      ioClient.emit('kirim', json);
-                     }
-                        driver.quit();
-                    });
-                });
-            });
-        }); 
-        }); 
-
-    });  
+                  console.log('Test passed = ', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                              fitur : ["login"],
+                              hasil : ["passed"]
+                            }              
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+                } else {
+                  console.log('Test failed =', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                                fitur : ["login"],
+                                hasil : ["fail"]
+                             }
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+              }
+              });
+            driver.findElement(webdriver.By.linkText("Saldo Tabungan")).click().then(function(){
+            sleep()
+            driver.getTitle().then(function(title) {
+                if(title === title) {
+                  console.log('Test passed = ', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                              fitur : ["click_saldo"],
+                              hasil : ["passed"]
+                            }              
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+                } else {
+                  console.log('Test failed =', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                                fitur : ["click_saldo"],
+                                hasil : ["fail"]
+                             }
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+              }
+              });
+            driver.findElement(webdriver.By.css("li:nth-child(2) > a")).click().then(function(){
+            sleep()
+            driver.getTitle().then(function(title) {
+                if(title === title) {
+                  console.log('Test passed = ', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                              fitur : ["click"],
+                              hasil : ["passed"]
+                            }              
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+                } else {
+                  console.log('Test failed =', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                                fitur : ["click"],
+                                hasil : ["fail"]
+                             }
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+              }
+              });
+            driver.findElement(webdriver.By.css("li:nth-child(3) > a")).click().then(function(){
+            sleep()
+            driver.getTitle().then(function(title) {
+                if(title === title) {
+                  console.log('Test passed = ', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                              fitur : ["click"],
+                              hasil : ["passed"]
+                            }              
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+                } else {
+                  console.log('Test failed =', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                                fitur : ["click"],
+                                hasil : ["fail"]
+                             }
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+              }
+              });
+            driver.findElement(webdriver.By.css("li:nth-child(4) > a")).click().then(function(){
+            sleep()
+            driver.getTitle().then(function(title) {
+                if(title === title) {
+                  console.log('Test passed = ', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                              fitur : ["click"],
+                              hasil : ["passed"]
+                            }              
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+                } else {
+                  console.log('Test failed =', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                                fitur : ["click"],
+                                hasil : ["fail"]
+                             }
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+              }
+              });
+            driver.findElement(webdriver.By.css("li:nth-child(5) > a")).click().then(function(){
+            sleep()
+            driver.getTitle().then(function(title) {
+                if(title === title) {
+                  console.log('Test passed = ', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                              fitur : ["click"],
+                              hasil : ["passed"]
+                            }              
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+                } else {
+                  console.log('Test failed =', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                                fitur : ["click"],
+                                hasil : ["fail"]
+                             }
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+              }
+              });
+            driver.findElement(webdriver.By.css("li:nth-child(6) > a")).click().then(function(){
+            sleep()
+            driver.getTitle().then(function(title) {
+                if(title === title) {
+                  console.log('Test passed = ', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                              fitur : ["click"],
+                              hasil : ["passed"]
+                            }              
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+                } else {
+                  console.log('Test failed =', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                                fitur : ["click"],
+                                hasil : ["fail"]
+                             }
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+              }
+              });
+            driver.findElement(webdriver.By.css("li:nth-child(7) > a")).click().then(function(){
+            sleep()
+            driver.getTitle().then(function(title) {
+                if(title === title) {
+                  console.log('Test passed = ', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                              fitur : ["click"],
+                              hasil : ["passed"]
+                            }              
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+                } else {
+                  console.log('Test failed =', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                                fitur : ["click"],
+                                hasil : ["fail"]
+                             }
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+              }
+              });
+            driver.findElement(webdriver.By.css("form > input:nth-child(1)")).click()
+            sleep()
+            driver.getTitle().then(function(title) {
+                if(title === title) {
+                  console.log('Test passed = ', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                              fitur : ["click"],
+                              hasil : ["passed"]
+                            }              
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+                } else {
+                  console.log('Test failed =', title);
+                  ioClient.emit ('judul', title);
+                  var json = {nama_web : [nama],
+                                fitur : ["click"],
+                                hasil : ["fail"]
+                             }
+                  console.log(json);
+                  ioClient.emit('kirim', json);
+              }
+              });
+        });
+    });
+});
+});
+});
+});
+});
+});
+});  
+});  
 }
 pindad();
